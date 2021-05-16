26816886a864cd59a957df970843b30ad16bc4ff
lukas <np-hardass@protonmail.com>
2021-05-17 00:37:20 +0200

    Concurrent type bound to Applicative

--- src/main/scala/io/github/paoloboni/http/ratelimit/package.scala
+++ src/main/scala/io/github/paoloboni/http/ratelimit/package.scala
@@ -21,13 +21,13 @@
 
 package io.github.paoloboni.http
 
-import cats.Monad
+import cats.Applicative
 import cats.effect.Concurrent
 import cats.implicits._
 import log.effect.LogWriter
 
 package object ratelimit {
-  implicit class LimiterOps[F[_]: Concurrent: LogWriter](limiter: RateLimiter[F]) {
+  implicit class LimiterOps[F[_]: Applicative: LogWriter](limiter: RateLimiter[F]) {
     def await[A](
         job: F[A],
         weight: Int = 1
