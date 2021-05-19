a230e5f0b47925c7bb2ccb199bda633c7c7955f5
lukas <np-hardass@protonmail.com>
2021-05-19 17:20:13 +0200

    added e2e test for order cancelation

--- src/e2e/scala/io/github/paoloboni/E2ETests.scala
+++ src/e2e/scala/io/github/paoloboni/E2ETests.scala
@@ -1,6 +1,7 @@
 package io.github.paoloboni
 
 import cats.effect.IO
+import cats.implicits._
 import cats.effect.testing.scalatest.AsyncIOSpec
 import io.github.paoloboni.TestConfig.config
 import io.github.paoloboni.binance._
@@ -52,5 +53,73 @@ class E2ETests extends AsyncFreeSpec with AsyncIOSpec with Matchers with Env {
         )
       )
       .asserting(_ shouldBe a[binance.OrderId])
+  }
+
+  "deleteOrder" in {
+    val side = Random.shuffle(OrderSide.values).head
+    BinanceClient[IO](config)
+      .use(client =>
+        for {
+          id <- client.createOrder(
+            OrderCreate(
+              symbol = "XRPUSDT",
+              side = side,
+              `type` = OrderType.MARKET,
+              timeInForce = None,
+              quantity = 100,
+              price = None,
+              newClientOrderId = None,
+              stopPrice = None,
+              icebergQty = None,
+              newOrderRespType = None
+            )
+          )
+
+          _ <- client.cancelOrder(
+            OrderCancel(
+              symbol = "XRPUSDT",
+              orderId = id.toLong.some,
+              origClientOrderId = None
+            )
+          )
+        } yield ()
+      )
+      .redeem(
+        _ => false,
+        _ => true
+      )
+      .asserting(_ shouldBe true)
+  }
+
+  "deleteAllOrders" in {
+    val side = Random.shuffle(OrderSide.values).head
+    BinanceClient[IO](config)
+      .use(client =>
+        for {
+          _ <- client.createOrder(
+            OrderCreate(
+              symbol = "XRPUSDT",
+              side = side,
+              `type` = OrderType.MARKET,
+              timeInForce = None,
+              quantity = 100,
+              price = None,
+              newClientOrderId = None,
+              stopPrice = None,
+              icebergQty = None,
+              newOrderRespType = None
+            )
+          )
+
+          _ <- client.cancelAllOrders(
+            OrderCancelAll(symbol = "XRPUSDT")
+          )
+        } yield ()
+      )
+      .redeem(
+        _ => false,
+        _ => true
+      )
+      .asserting(_ shouldBe true)
   }
 }
