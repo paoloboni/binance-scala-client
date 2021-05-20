a81e46f1ca5a99960ff7540c480f5ca6fd10d447
lukas <np-hardass@protonmail.com>
2021-05-20 16:46:03 +0200

    added filter types and decoders

---
+++ src/main/scala/io/github/paoloboni/binance/common/response/ExchangeInformation.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Paolo Boni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.paoloboni.binance.common.response
+
+import io.circe.generic.extras.Configuration
+
+sealed trait Filter
+
+case class PRICE_FILTER(minPrice: BigDecimal, maxPrice: BigDecimal, tickSize: BigDecimal)              extends Filter
+case class PERCENT_PRICE(multiplierUp: BigDecimal, multiplierDown: BigDecimal, multiplierDecimal: Int) extends Filter
+case class LOT_SIZE(minQty: BigDecimal, maxQty: BigDecimal, stepSize: BigDecimal)                      extends Filter
+case class MARKET_LOT_SIZE(minQty: BigDecimal, maxQty: BigDecimal, stepSize: BigDecimal)               extends Filter
+case class MAX_NUM_ORDERS(limit: Int)                                                                  extends Filter
+case class MAX_NUM_ALGO_ORDERS(limit: Int)                                                             extends Filter
+case class MIN_NOTIONAL(notional: Int)                                                                 extends Filter
+
+object FilterJsonConfiguration {
+  implicit val genDevConfig: Configuration = Configuration.default.withDiscriminator("filterType")
+}
---
+++ src/test/scala/io/github/paoloboni/binance/spot/response/FilterTypeJsonTest.scala
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2021 Paolo Boni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.paoloboni.binance.common.response
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import io.circe.parser.decode
+import io.circe.generic.extras.auto._
+import io.circe.parser
+import io.circe.syntax._
+
+class FilterJsonTests extends AnyFlatSpec with Matchers {
+
+  "PERCENT_PRICE" should "be able to being decoded from json" in {
+
+    import FilterJsonConfiguration._
+
+    val actual = decode[Filter]("""
+                   |{
+                   |  "filterType": "PERCENT_PRICE",
+                   |  "multiplierUp": "1.1500",
+                   |  "multiplierDown": "0.8500",
+                   |  "multiplierDecimal": 4
+                   |}
+                   """.stripMargin)
+
+    val expected = Right(
+      PERCENT_PRICE(
+        multiplierUp = 1.1500,
+        multiplierDown = 0.8500,
+        multiplierDecimal = 4
+      )
+    )
+
+    actual shouldBe expected
+  }
+
+  "MIN_NOTIONAL" should "be able to being decoded from json" in {
+
+    import FilterJsonConfiguration._
+
+    val actual = decode[Filter]("""
+                   |{
+                   |  "filterType": "MIN_NOTIONAL",
+                   |  "notional": "1"
+                   |}
+                   """.stripMargin)
+
+    val expected = Right(
+      MIN_NOTIONAL(
+        notional = 1
+      )
+    )
+
+    actual shouldBe expected
+  }
+
+  "MAX_NUM_ALGO_ORDERS" should "be able to being decoded from json" in {
+
+    import FilterJsonConfiguration._
+
+    val actual = decode[Filter]("""
+                   |{
+                   |  "filterType": "MAX_NUM_ALGO_ORDERS",
+                   |  "limit": 100
+                   |}
+                   """.stripMargin)
+
+    val expected = Right(MAX_NUM_ALGO_ORDERS(limit = 100))
+
+    actual shouldBe expected
+  }
+
+  "MAX_NUM_ORDERS" should "be able to being decoded from json" in {
+
+    import FilterJsonConfiguration._
+
+    val actual = decode[Filter]("""
+                   |{
+                   |  "filterType": "MAX_NUM_ORDERS",
+                   |  "limit": 100
+                   |}
+                   """.stripMargin)
+
+    val expected = Right(MAX_NUM_ORDERS(limit = 100))
+
+    actual shouldBe expected
+  }
+}
