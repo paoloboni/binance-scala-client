1082fa97ac40c7ffa4ce1521aa52118963e39d99
Paolo Boni <boni.paolo@gmail.com>
2022-08-18 14:08:17 +0200

    Use gpg key in release.yml

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-: "${SONATYPE_USERNAME?must be defined}"
-: "${SONATYPE_PASSWORD?must be defined}"
-
-export GPG_TTY=$(tty)
-sbt "release cross with-defaults skip-tests"
-sbt ++3.1.3 "doc;ghpagesPushSite"
