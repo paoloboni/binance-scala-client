dd18ddcbffafa9e785983a0547a78175405cd121
Paolo Boni <4746479+paoloboni@users.noreply.github.com>
2023-01-01 15:22:22 +0100

    Update headers

--- src/main/scala/io/github/paoloboni/binance/common/response/Ask.scala
+++ src/main/scala/io/github/paoloboni/binance/common/response/Ask.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Paolo Boni
+ * Copyright (c) 2023 Paolo Boni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
